e67a7ae1629b86ca1d935d32aef549641d2d6f45
Florent Poujol <florent.poujol@gmail.com>
2015-02-28 14:15:29 +0100

    Cleaned up repo.

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,24 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-var htmlreplace = require('gulp-html-replace');
-var concat = require('gulp-concat');
 var insert = require('gulp-insert');
 
 var metadata = require('./package');
 var header = '// ' + metadata.name + ' v' + metadata.version + ' ' + metadata.homepage + '\n';
 
 gulp.task('lint', function() {
-	return gulp.src('src/Tween.js')
+	return gulp.src('src/sp-tweenjs.js')
 	.pipe(jshint())
 	.pipe(jshint.reporter('default'));
 });
 
 gulp.task('min', function() {
 	return gulp.src([
-		'src/Tween.js'
+		'src/sp-tweenjs.js'
 	])
 	.pipe(uglify())
 	.pipe(insert.prepend(header))
-	.pipe(rename('tween.min.js'))
+	.pipe(rename('sp-tweenjs.min.js'))
 	.pipe(gulp.dest('build'));
 });
 
--- test/unit/nodeunitheadless.js
+++ test/unit/nodeunitheadless.js
@@ -1,10 +1,10 @@
 // We will unit test both the original library and the minified version
-var TWEEN_uncompressed = require('../../src/Tween.js');
-var TWEEN_min = require('../../build/tween.min.js');
+var SPTWEEN_uncompressed = require('../../src/sp-tweenjs.js');
+// var TWEEN_min = require('../../build/tween.min.js');
 var getTests = require('./tests');
 
 module.exports = {
-	'tween': getTests(TWEEN_uncompressed),
+	'tween': getTests(SPTWEEN_uncompressed),
 	// 'tween_min': getTests(TWEEN_min)
 };
 
